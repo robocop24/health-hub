0169bf0e29fc970e14d2f82b9c1193d07046444e
robocop24 <suhailriyaz94@gmail.com>
2024-09-20 12:33:15 +0530

    bug fixed

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from '../components/Header'
 import TopDoctors from '../components/TopDoctors'
+import SpecialityMenu from '../components/SpecialityMenu'
 import Banner from '../components/Banner'
 
 const Home = () => {
@@ -8,6 +9,7 @@ const Home = () => {
     <div>
       <Header/>
       <TopDoctors/>
+      <SpecialityMenu/>
       <Banner/>
    </div>
   )
